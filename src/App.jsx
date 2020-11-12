e34455a7ae87ce50d8862aa7b800ad66a36adde0
Jose Morales <jmorales072095@gmail.com>
2020-11-12 14:05:02 -0800

    added friends page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@
 import React from "react";
 import "./App.css";
 import UserSign from "./pages/SignUp";
-import Home from "../src/pages/Home";
+import FriendsPage from "./pages/friends"
+import Home from "./pages/Home";
 //import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <UserSign />
+      {/* <UserSign /> */}
+      <FriendsPage />
     </div>
   );
 }
