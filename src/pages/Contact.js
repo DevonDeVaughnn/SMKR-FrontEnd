4327754d43a19cd952a0cf246b34610de1eb199e
“karzoun” <karzoun_27@hotmail.com>
2020-11-17 10:23:29 -0800

    overview of actions

---
+++ src/pages/Contact.js
@@ -0,0 +1,114 @@
+import React, { Component, useState } from "react";
+
+import { useHistory } from "react-router-dom";
+import "../components/Form/formStyle.css";
+import { Container, Form } from "../components/Grid";
+
+class ContactUs extends Component {
+  render() {
+    return (
+      <Container
+        fluid
+        style={{
+          width: "100%",
+          color: "black",
+
+          flex: 1,
+          flexDirection: "column",
+        }}
+      >
+        <div className="card" style={{ width: "24rem" }}>
+          <Form id="form">
+            <div>
+              <div className="card-img-top myImage"></div>
+              <br></br>
+            </div>
+            <h4>Welcome to SMKR. </h4>
+            <p>Write to us.</p>
+
+            <div
+              className="card-body text-center "
+              style={{
+                color: "black",
+              }}
+            >
+              <label htmlFor="username">Username: </label>
+              <input
+                style={{
+                  marginLeft: "1em",
+                  border: "none",
+                }}
+                type="text"
+                id="username"
+                name="username"
+              />
+
+              <div
+                className="card-body text-center"
+                style={{
+                  color: "black",
+                }}
+              >
+                <label htmlFor="email">E-Mail: </label>
+                <input
+                  style={{
+                    textAlign: "left",
+                    marginLeft: "1em",
+                    border: "none",
+                  }}
+                  type="text"
+                  id="email"
+                  name="email"
+                  // onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div
+                className="card-body text-center"
+                style={{
+                  color: "black",
+                }}
+              >
+                <label htmlFor="message">Message: </label>
+                <input
+                  style={{
+                    textAlign: "center",
+                    marginLeft: "1em",
+                    border: "none",
+                  }}
+                  type="text"
+                  id="message"
+                  name="message"
+                />
+              </div>
+
+              <div
+                className="card-body text-center"
+                style={{
+                  color: "black",
+                }}
+              ></div>
+
+              <button
+                className="btn btn-primary"
+                type="submit"
+                //onClick={}
+                //disabled={parseInt(user.age) < 21 ? true : false}
+                style={{
+                  textAlign: "center",
+                  position: "center",
+                  border: "none",
+
+                  fontFamily: "Playfair Display, serif",
+                }}
+              >
+                <i className="fas fa-cannabis"></i>
+              </button>
+            </div>
+          </Form>
+        </div>
+      </Container>
+    );
+  }
+}
+
+export default ContactUs;
