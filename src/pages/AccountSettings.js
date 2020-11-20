3463f5976661c800facccb4dbe0ae91b3b842b67
“karzoun” <karzoun_27@hotmail.com>
2020-11-20 13:43:17 -0800

    userAccount

--- src/pages/AccountSettings.js
+++ src/pages/AccountSettings.js
@@ -1,26 +1,48 @@
 import React, { Component } from "react";
 import axios from "axios";
+// import User from "../../../SMKR-BackEnd/models";
 import { Row, Container, Form, Col, Button, Card } from "react-bootstrap";
 import LoginJumbotron from "../components/Jumbotron/loginJumbotron";
 import "../components/Form/form.css";
 
 //import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-export default class Login extends Component {
-  handleSubmit = (e) => {
-    e.preventDefault();
-    const data = {
-      username: this.username,
-      password: this.password,
-    };
-    axios
-      .post("login", data)
-      .then((res) => {
-        localStorage.setItem("token", res.data.token);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+export default class AccountSetting extends Component {
+  state = {
+    oldEmail: "",
+    newEmail: "",
+    oldPassword: "",
+    newPassword: "",
+    newUserName: "",
+    oldUserName: "",
+  };
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+  handleUpdateEmail = async () => {
+    // console.log(this.state.oldEmail, this.state.newEmail);
+    const res = await axios.put("/updateEmail", {
+      newEmail: this.state.newEmail,
+      oldEmail: this.state.oldEmail,
+    });
+  };
+  handleUpdatePassword = async () => {
+    // console.log(this.state.oldPassword, this.state.newPassword);
+    const res = await axios.put("/updatePassword", {
+      newPassword: this.state.newPassword,
+      oldPassword: this.state.oldPassword,
+    });
+  };
+  handleUpdateUserName = async () => {
+    // console.log(this.state.oldUserName, this.state.newUserName);
+    const res = await axios.put("/updateUserName", {
+      newUserName: this.state.newUserName,
+      oldUserName: this.state.oldUserName,
+    });
+  };
+  handleDeleteUser = async () => {
+    const res = await axios.delete("/deleteUser", { data: this.state.id });
   };
   render() {
     return (
@@ -30,16 +52,99 @@ export default class Login extends Component {
         </Row>
         <Row>
           <Col size="md-12" className="column">
-            <Button variant="primary" size="lg" block>
-              Update username
-            </Button>
-            <Button variant="primary" size="lg" block>
-              Update email
-            </Button>
-            <Button variant="primary" size="lg" block>
-              Update password
-            </Button>
-            <Button variant="danger" size="lg" block>
+            <Form.Group onSubmit={this.handleSubmit}>
+              <h1>Update Username</h1>
+              <label> Old Username</label>
+              <Form.Control
+                name="oldUserName"
+                value={this.state.oldUserName}
+                type="text"
+                placeholder=" old Username"
+                onChange={this.handleChange}
+              />
+              <br />
+              <label>New Username</label>
+              <Form.Control
+                name="newUserName"
+                value={this.state.newUserName}
+                type="text"
+                placeholder="new Username"
+                onChange={this.handleChange}
+              />
+              <br />
+              <Button
+                onClick={this.handleUpdateUserName}
+                variant="primary"
+                size="lg"
+                block
+              >
+                Update Username
+              </Button>
+            </Form.Group>
+            <h1>Update Email</h1>
+            <Form.Group>
+              <label>old Email</label>
+              <Form.Control
+                name="oldEmail"
+                value={this.state.oldEmail}
+                type="text"
+                placeholder="oldEmail@email.com"
+                onChange={this.handleChange}
+              />
+              <br />
+              <label>New Email</label>
+              <Form.Control
+                name="newEmail"
+                value={this.state.newEmail}
+                type="text"
+                placeholder="newEmail@email.com"
+                onChange={this.handleChange}
+              />
+              <br />
+              <Button
+                onClick={this.handleUpdateEmail}
+                variant="primary"
+                size="lg"
+                block
+              >
+                Update Email
+              </Button>
+            </Form.Group>
+            <h1>Update Password</h1>
+            <Form.Group onSubmit={this.handleSubmit}>
+              <label> Old Password</label>
+              <Form.Control
+                name="oldPassword"
+                value={this.state.oldPassword}
+                type="password"
+                placeholder=" old Password"
+                onChange={this.handleChange}
+              />
+              <br />
+              <label> New Password</label>
+              <Form.Control
+                name="newPassword"
+                value={this.state.newPassword}
+                type="password"
+                placeholder=" new Password"
+                onChange={this.handleChange}
+              />
+              <br />
+              <Button
+                onClick={this.handleUpdatePassword}
+                variant="primary"
+                size="lg"
+                block
+              >
+                Update password
+              </Button>
+            </Form.Group>
+            <Button
+              onClick={this.handleDeleteUser}
+              variant="danger"
+              size="lg"
+              block
+            >
               Delete account
             </Button>
           </Col>
@@ -48,3 +153,12 @@ export default class Login extends Component {
     );
   }
 }
+
+// const id = req.params.id;
+// User.findByIdAndDelete(id, function (err, user) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("deleted", user);
+//   }
+// });
